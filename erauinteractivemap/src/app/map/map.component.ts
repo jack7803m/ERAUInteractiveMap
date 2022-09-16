9cfe55c23f285c7951372d5e3658022d29f0127d
DESKTOP-CRVK0T0\westi <Westin.Pellicer@onesky.com>
2022-09-16 16:36:03 -0400

    Creating Branch

--- erauinteractivemap/src/app/map/map.component.ts
+++ erauinteractivemap/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { RotatingMapOptions } from 'leaflet-rotate-map';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +10,13 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
 
   constructor() { }
+
+  layer =  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 25, attribution: '...' });
+  center = L.latLng({ lat: 29.188943840305406, lng: -81.04962629704922 });
+
+  map = L.map('map', {rotate:true})
+    .addLayer(this.layer)
+    .setView(this.center);
 
   public options: L.MapOptions = {
     layers: [
