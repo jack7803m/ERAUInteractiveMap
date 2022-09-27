9e4492422e23fe6dfdea4a06d1ca12eaf23b833a
jack-morris <jack.morris@onesky.com>
2022-09-27 09:31:54 -0400

    error was self-imposed, raised build size limit

--- erauinteractivemap/tailwind.config.js
+++ erauinteractivemap/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
