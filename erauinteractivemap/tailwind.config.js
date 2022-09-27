c125721652db0844b6b29ecf62ff7af31a6a4a3c
jack-morris <jack.morris@onesky.com>
2022-09-27 09:11:37 -0400

    lower build size with tailwind

--- erauinteractivemap/tailwind.config.js
+++ erauinteractivemap/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
