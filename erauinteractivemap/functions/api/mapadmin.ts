43a44d3dcdda8afdd0a9ed9239c0b9501cd15b0b
Jack7803M <jack@liviewater.com>
2022-10-02 01:34:58 -0400

    fix functions

--- erauinteractivemap/functions/api/mapadmin.ts
+++ erauinteractivemap/functions/api/mapadmin.ts
@@ -1,19 +1,9 @@
-import { Building, Point } from "src/app/models/mapData.model";
-
 export async function onRequestPost(context: any): Promise<Response> {
     const { request, env, params, waitUntil, next, data } = context;
 
     const database = env.KV_MAPDB as KVNamespace;
 
-    let fakeData = new Array<Building>();
-    let fakeBuilding = new Building(new Point(0, 0), "Building 1", "This is a building");
-    fakeBuilding.entrances = new Array<Point>();
-    fakeBuilding.entrances.push(new Point(0, 0));
-    fakeBuilding.entrances.push(new Point(0, 1));
-    fakeBuilding.entrances.push(new Point(0, 2));
-    fakeBuilding.entrances.push(new Point(0, 3));
-
-    fakeData.push(fakeBuilding);
+    let fakeData = "asdalsdkm";
 
     await database.put('data', JSON.stringify(fakeData));1
 
