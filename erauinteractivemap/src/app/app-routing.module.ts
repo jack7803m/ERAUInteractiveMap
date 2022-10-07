e78e4c1725e9c16f1bc32d9252873a317527946a
Micah Parrilla <micahparrilla@icloud.com>
2022-10-07 00:18:30 -0400

    Added Route to page by micah

--- erauinteractivemap/src/app/app-routing.module.ts
+++ erauinteractivemap/src/app/app-routing.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminEditComponent } from './admin/admin-edit/admin-edit.component';
 import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
 import { MapComponent } from './map/map.component';
+import { AuthGuard } from './_guards/auth.guard';
 import { UnauthGuard } from './_guards/unauth.guard';
 
 const routes: Routes = [
-  { path: '', component: MapComponent, },
-  {
-    path: 'admin/login',
-    component: AdminLoginComponent,
-    canActivate: [UnauthGuard],
-  },
-  {
-    path: '**',
-    component: MapComponent,
-  },
+    { path: '', component: MapComponent },
+    {
+        path: 'admin/login',
+        component: AdminLoginComponent,
+        canActivate: [UnauthGuard],
+    },
+    {
+        path: 'admin/edit',
+        component: AdminEditComponent,
+        // canActivate: [AuthGuard],
+    },
+    {
+        path: '**',
+        component: MapComponent,
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
