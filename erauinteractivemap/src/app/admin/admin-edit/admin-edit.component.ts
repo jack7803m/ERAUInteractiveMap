09e9169135a897e8e496cec856c46cd186abf543
Micah Parrilla <micahparrilla@icloud.com>
2022-10-06 22:43:01 -0400

    Creating edit files

---
+++ erauinteractivemap/src/app/admin/admin-edit/admin-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit',
+  templateUrl: './admin-edit.component.html',
+  styleUrls: ['./admin-edit.component.scss']
+})
+export class AdminEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- erauinteractivemap/src/app/app.module.ts
+++ erauinteractivemap/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { AdminEditComponent } from './admin/admin-edit/admin-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  declarations: [AppComponent, MapComponent, AdminEditComponent],
   imports: [BrowserModule, AppRoutingModule, LeafletModule],
   providers: [],
   bootstrap: [AppComponent],
